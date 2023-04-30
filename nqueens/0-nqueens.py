34f91383e13796858bdde501d39b528d707fb8e4
Karolart <karoljuzz1610@gmail.com>
2023-04-30 00:38:20 -0500

    Nqueens

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,62 +1,15 @@
 #!/usr/bin/python3
-""" The N queens puzzle ""
-import sys
-
-class NQueen:
-    """ handless Nqueen problem """
-
-    def __init__(self, n):
-
-        self.n = n
-        self.x = [0 for i in range(n + 1)]
-        self.res = []
-
-    def place(self, k, i):
-        """ select the rigth place for the queen
-        """
-        for j in range(1, k):
-
-            if self.x[j] == i or \
-               abs(self.x[j] - i) == abs(j - k):
-                return 0
-        return 1
-
-    def nQueen(self, k):
-        """ Tries to place every queen in the board
-        Args:
-        k: starting queen from which to evaluate (should be 1)
-        """
-        for i in range(1, self.n + 1):
-            if self.place(k, i):
-                self.x[k] = i
-                if k == self.n:
-                    solution = []
-                    for i in range(1, self.n + 1):
-                        solution.append([i - 1, self.x[i] - 1])
-                    self.res.append(solution)
-                else:
-                    self.nQueen(k + 1)
-        return self.res
+"""The N queens puzzle is the challenge of placing
+   N non-attacking queens on an N×N chessboard"""
+from sys import argv
 
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-
-N = sys.argv[1]
-
-try:
-    N = int(N)
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
-
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-queen = NQueen(N)
-res = queen.nQueen(1)
-
-for i in res:
-    print(i)
+if (len(argv) != 2):
+    print('Usage: nqueens N')
+    exit(1)
+elif not argv[1].isdigit():
+    print('N must be a number')
+    exit(1)
+elif int(argv[1]) < 4:
+    print('N must be at least 4')
+    exit(1)
