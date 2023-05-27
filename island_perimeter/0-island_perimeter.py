d33b2762accc01752443fcd29edd231cd880f440
Karolart <karoljuzz1610@gmail.com>
2023-05-26 22:55:39 -0500

    Island perimeter

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Defines function to calculate the perimeter of an island
+represented by a list of list of ints
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    rows = len(grid)
+    cols = len(grid[0])
+
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
