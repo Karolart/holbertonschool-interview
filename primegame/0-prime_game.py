ebaf6fd771d8f598b94af123a71f02720a3e9115
Karolart <karoljuzz1610@gmail.com>
2023-06-02 23:17:10 -0500

    primeGame

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
 
 """
-   Maria always goes first and both players play optimally,
-   determine who the winner of each game is.
+Defines function that determines the winner after a certain number of rounds
+of playing the Prime Game
 """
 
 
 def isWinner(x, nums):
-    # Define a helper function to check if a number is prime
+    """
+    Determines the winner of a game played for multiple rounds.
+
+    Args:
+        x (int): Number of rounds to be played.
+        nums (list): Array of 'n' values for each round.
+
+    Returns:
+        str or None: The name of the player that won the most rounds ('Maria' or 'Ben'), or None if the winner cannot be determined.
+    """
+
     def isPrime(num):
+        """
+        Checks if a number is prime.
+
+        Args:
+            num (int): The number to be checked.
+
+        Returns:
+            bool: True if the number is prime, False otherwise.
+        """
         if num < 2:
             return False
         for i in range(2, int(num**0.5) + 1):
@@ -16,13 +35,20 @@ def isWinner(x, nums):
                 return False
         return True
 
-    # Define the recursive game function
     def playGame(n, isMariaTurn):
-        # Base case: If n = 1, the current player loses
-        if n == 1:
+        """
+        Simulates the game for a given number 'n' and player's turn.
+
+        Args:
+            n (int): The current number.
+            isMariaTurn (bool): True if it's Maria's turn, False otherwise.
+
+        Returns:
+            bool: True if the current player can force a win, False otherwise.
+        """
+        if n == 1:  # Base case: No prime numbers left to choose
             return not isMariaTurn
 
-        # Iterate through the remaining numbers and choose a prime
         for i in range(2, n + 1):
             if isPrime(i):
                 # Remove the prime number and its multiples
@@ -33,21 +59,19 @@ def isWinner(x, nums):
 
         return False
 
-    # Count the number of wins for each player
     maria_wins = 0
     ben_wins = 0
 
-    # Play the game for each round
     for n in nums:
         if playGame(n, True):
             maria_wins += 1
         else:
             ben_wins += 1
 
-    # Determine the winner or return None if it cannot be determined
     if maria_wins > ben_wins:
         return "Maria"
     elif maria_wins < ben_wins:
         return "Ben"
     else:
         return None
+    
